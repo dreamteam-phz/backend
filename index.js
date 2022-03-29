f40f1c82c4270e5a7369d9b70bd61f4fc66a0c3b
abel-parada <abel.parada.millan@gmail.com>
2022-03-29 13:24:49 +0300

    created:frontend to post to database

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const { render } = require('express/lib/response');
 const mongoose = require('mongoose');
+const path = require('path');
 
 //import routes
 const routes = require('./routes/routes.js');
 
 //require the contents of our .env file
-
 require('dotenv').config();
 const mongoString = process.env.DATABASE_URL;
 
@@ -20,15 +21,19 @@ database.once('connected', () => {
     console.log('Database Connected');
 });
 
+//end of database connection
+
+//build up our middleware
 const app = express();
 const host  = 'https://localhost';
 const port = 3000;
-
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use('/api',routes);
 
 app.get('/', (req,res) => {
-    res.send('Database is connected. Amazing!');
+    res.sendFile(path.join(__dirname,'index.html'));
 })
 
 app.listen((host,port), ()=>{console.log(`Server listening at ${host}:${port}`)});
