857ce335db18cb359b608bcfc9f4733819289666
Ouss84 <bahrioussama84@gmail.com>
2022-05-05 22:05:11 +0300

    setting jest + supertest + restructuring the server access + running tests to check connection to db + verifying existing surveys

---
+++ __tests__/routes.test.js
@@ -0,0 +1,24 @@
+const app = require("../index"); // Link to the server file
+const supertest = require("supertest");
+
+describe("Surveys", () => {
+  describe("get survey route", () => {
+    describe("getting surveys", () => {
+      it("Should return the number of surveys", async () => {
+        await supertest(app).get("/api/surveys").expect(200);
+      });
+    });
+    describe("Check if a survey doesn't exists in the DB", () => {
+      it("should return error:500", async () => {
+        const surveyId = "123456";
+        await supertest(app).get(`/api/surveys/${surveyId}`).expect(500);
+      });
+    });
+    describe("Check if an added survey exists in the DB", () => {
+      it("should return OK:200", async () => {
+        const surveyId = "6271070e8a4158cc57c9124b";
+        await supertest(app).get(`/api/surveys/${surveyId}`).expect(200);
+      });
+    });
+  });
+});
